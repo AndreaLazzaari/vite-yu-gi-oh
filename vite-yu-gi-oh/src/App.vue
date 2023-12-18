9ead44bd34c139e3d6471202870e1169053d6f71
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-12-18 16:06:24 +0100

    inizializzazione esercizio, con inserimento di bootstrap e sass

--- vite-yu-gi-oh/src/App.vue
+++ vite-yu-gi-oh/src/App.vue
@@ -1,30 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<template lang="">
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<AppHeader/>
+
+<main>
+
+</main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import AppHeader from '../src/components/AppHeader.vue';
+
+export default {
+  data() {
+    return {
+      components: {
+        AppHeader
+      },
+    }
+  },
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+</script>
+<style lang="scss">
+  @use './styles/general.scss';
+
+  main{
+    background-color: #d48f38;
+    height: 100vh;
+  }
 </style>
---
+++ vite-yu-gi-oh/src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template lang="">
+  <header></header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" store>
+@use '../styles/general.scss' as *;
+
+header{
+    height: 80px;
+    width: 100%;
+    background-color: aqua;
+}
+    
+</style>
